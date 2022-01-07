91bbf3cc0343b2fea57a35defe00fac21bff172c
Zibbp <developerzippy@gmail.com>
2022-01-07 15:27:44 -0600

    Add dark mode color text for video title

--- components/Video/VideoTitle.js
+++ components/Video/VideoTitle.js
@@ -25,7 +25,7 @@ const VideoTitle = ({ data }) => {
               text-sm text-gray-600
               sm:py-2 sm:mt-0
               mt-2
-              dark:text-true-gray-200
+              dark:text-neutral-200
             "
                 >
                     <Link href={`/channels/${data.video.uploader}`}>
@@ -84,9 +84,10 @@ const VideoTitle = ({ data }) => {
               mt-4
               justify-center
               sm:justify-start
+              dark:text-neutral-200
             "
                 >
-                    <a className="text-gray-500 dark:text-true-gray-200">
+                    <a className="text-gray-500 dark:text-neutral-200">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             className="h-5 w-5"
@@ -100,12 +101,12 @@ const VideoTitle = ({ data }) => {
                             <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path>
                         </svg>
                     </a>
-                    <a className="ml-2 text-gray-500 dark:text-true-gray-200">
+                    <a className="ml-2 text-gray-500 dark:text-neutral-200">
                         {data.video.like_count.toLocaleString()}
                     </a>
                     {data.video.dislike_count > 0 && (
                         <>
-                            <a className="ml-3 text-gray-500 dark:text-true-gray-200">
+                            <a className="ml-3 text-gray-500 dark:text-neutral-200">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     width="24"
@@ -121,13 +122,13 @@ const VideoTitle = ({ data }) => {
                                     <path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path>
                                 </svg>
                             </a>
-                            <a className="ml-2 text-gray-500 dark:text-true-gray-200">
+                            <a className="ml-2 text-gray-500 dark:text-neutral-200">
                                 {data.video.dislike_count.toLocaleString()}
                             </a>
                         </>
                     )}
                     <a
-                        className="ml-3 text-gray-500 dark:text-true-gray-200"
+                        className="ml-3 text-gray-500 dark:text-neutral-200"
                         href={data.httpVideoUrl}
                         title="Download Video"
                         target="_blank"
